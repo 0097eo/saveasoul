938b1a58360b34eaf5ba6e72497e2673f0e84c39
Emmanuel <emmanuelokello294@gmail.com>
2024-04-02 10:25:42 +0300

    creates json file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function renderOneAnimal(animal){
     let card = document.createElement('li')
     card.className = 'card'
     card.innerHTML = `
-    <img src="${animal.imageUrl} alt="img">
+    <img src=${animal.imageUrl} alt="img">
     <div class="content">
     <h4>${animal.name}</h4>
     <p>
@@ -15,10 +15,16 @@ function renderOneAnimal(animal){
    //add animal card to 
     document.querySelector('#animal-list').appendChild(card)
 }
+function getAllAnimals(){
+    fetch('http://localhost:3000/animalData')
+    .then(res=>res.json())
+    .then(animalData =>animalData.forEach(animal => renderOneAnimal(animal)))
+}
+
 //initial render
 //get data and load or render our animals to the Dom
 
 function initialize(){
-    animalData.forEach(animal => renderOneAnimal(animal))
+    getAllAnimals()
 }
 initialize()
